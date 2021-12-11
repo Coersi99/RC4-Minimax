0ad0479553f783df74ef775d6f6900d5061ff0fe
Nils Martel <nilsmartel@yahoo.de>
2021-12-11 12:44:01 +0100

    Test implementation

--- rc4_Nils.py
+++ rc4_Nils.py
@@ -8,9 +8,14 @@ def swap(list, i, j):
 
 
 class Rc4:
-    i = 0
-    j = 0
-    state = list(range(256))
+    def __init__(self, key):
+        self.i = 0
+        self.j = 0
+        self.state = list(range(256))
+
+        self.init_state(key)
+
+       
 
     def init_state(self, key):
         j = 0
@@ -18,7 +23,7 @@ class Rc4:
             s = self.state[i]
             k = key[i % len(key)]
 
-            j += s+k 
+            j += s+k
             j &= 0xff
 
             # swap elements at index i and j
@@ -37,6 +42,47 @@ class Rc4:
         index = self.state[self.i]+self.state[self.j]
         index &= 255
         return self.state[index]
-    
+
     def decrypt(self, input):
         return [i ^ self.prga_next() for i in input]
+
+
+# this is just used as a test function
+def main():
+    def to_byte(a, b):
+        return int(a+b, 16)
+
+    def to_bytes(s):
+        s = list(s)
+        return [to_byte(s[i-1], s[i]) for i in range(len(s)) if i & 1 == 1]
+
+    def test(key, input, expected):
+        key = to_bytes(key)
+        input = to_bytes(input)
+        expected = to_bytes(expected)
+
+        got = Rc4(key).decrypt(input)
+
+        if (got != expected):
+            print("[test failed] expected output didnt match actual output!")
+            return
+
+        print("[test passed]")       
+
+    test(
+        key="0102030405",
+        input="00000000000000000000000000000000",
+        expected="b2396305f03dc027ccc3524a0a1118a8"
+    )
+    test(
+        key="01020304050607",
+        input="00000000000000000000000000000000",
+        expected="293f02d47f37c9b633f2af5285feb46b"
+    )
+    test(
+        key="0102030405060708",
+        input="00000000000000000000000000000000",
+        expected="97ab8a1bf0afb96132f2f67258da15a8"
+    )
+
+main()
