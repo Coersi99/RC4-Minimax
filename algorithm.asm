a95fca812bf186b4a02ae939425cde915a547659
Falk Stock <falk.stock1@gmail.com>
2022-01-28 19:49:00 +0100

    Dokumentation + optimierte Minimax

--- algorithm.asm
+++ algorithm.asm
@@ -1,3 +1,38 @@
+; this is the 
+
+; some help for understanding our code:
+
+; a)    a << b  means a is shifted to the left by b bits, "empty" bits on the right are set to 0
+;       a >> b  means a is shifted to the right by b bits, "empty" bits on the left are set to 0 (unsigned mode)
+
+; b)    these equivalences are used in our programming, because they are valid for binary numbers and lower the "costs" of our program
+
+;       a mod b === a & (b-1)
+;       a / b === a >> b (while b is 2^n and n is a whole positive number)
+;       a | b === a + b
+
+
+
+////////////////////////////////////////
+
+; HOW TO USE OUR MINIMAX-PROGRAM:
+
+; import the sbox-file at the following adress: 00000
+; import the key-file at the following adress: 00050
+; import the data_encrypted-file at the following adress: 00060
+
+; run the program
+
+; export adresses 00060 to 000D54 into a file (.txt for the raw data or .jpeg for the visual result)
+
+; look at the file
+
+; be amazed
+
+//////////////////////////////////////////////
+
+; some variables we use:
+
 ; adress of the SBOX
 const SBOX_ADDR = 0
 
@@ -7,7 +42,7 @@ const KEY_LEN = 4
 
 ; adress and length of the input
 const INPUT_ADDR = 96 ;96dec <- 60hex 
-const INPUT_LENGTH =  13265
+const INPUT_LEN =  13265
 
 ; length of one word
 const WORD_LEN = 4
@@ -23,6 +58,12 @@ const delete_byte_4=-256 ;11111111 11111111 11111111 00000000
 const delete_byte_3=-65281 ;11111111 11111111 00000000 11111111
 const delete_byte_2=-16711681 ;11111111 00000000 11111111 11111111
 const delete_byte_1=16777215 ;00000000 11111111 11111111 11111111
+
+
+//////////////////////
+
+; this is where the fun begins
+
 
 ; visualization of the program sequence
 main:
@@ -47,7 +88,7 @@ get_key:
     MDR <- [MAR]
 
     ; i % keylength, depending on that call the right read function
-    ACCU <- i MOD 4
+    ACCU <- i & 3
 
     if ACCU <- 0: jump read_key_4
 
@@ -66,27 +107,27 @@ read_key_4:
 
 read_key_3:
     MDR <- 65280 & MDR
-    MDR <- 0^8@MDR[31..8]
+    MDR <- MDR >> 8
 
 read_key_2:
     MDR <- 16711680 & MDR
-    MDR <- 0^16@MDR[31..16]
+    MDR <- MDR >> 16
 
 read_key_1:
     MDR <- -16777216 & MDR
-    MDR <- 0^24@MDR[31..24]
+    MDR <- MDR >> 24
 
 ////////////////////////////
 get_Si:
     ; get correct memory cell and save the adress for later use
-    MAR <- i / 4
-    tmp_adr <- i / 4
+    MAR <- i >> 4
+    tmp_adr <- i >> 4
     
     ; load in data for the read function to use
     MDR <- [MAR]
 
     ; call the correct read function with the help of modulo
-    ACCU <- i MOD 4
+    ACCU <- i & 3
 
     if ACCU <- 0: jump read_Si_4
 
@@ -105,28 +146,28 @@ read_Si_4:
 
 read_Si_3:
     MDR <- 65280 & MDR
-    MDR <- 0^8@MDR[31..8]
+    MDR <- MDR >> 8
     tmp_Si <- MDR
 
 read_Si_2:
     MDR <- 16711680 & MDR
-    MDR <- 0^16@MDR[31..16]
+    MDR <- MDR >> 16
     tmp_Si <- MDR
 
 read_Si_1:
     MDR <- -16777216 & MDR
-    MDR <- 0^24@MDR[31..24]
+    MDR <- MDR >> 24
     tmp_Si <- MDR
 
 ///////////////////////
 get_Sj:
     ; get correct memory cell
-    MAR <- j / 4
+    MAR <- j >> 4
     ; load in data for the read function to use
     MDR <- [MAR]
 
     ; call the correct read function with the help of modulo
-    ACCU <- j MOD 4
+    ACCU <- j & 3
 
     if ACCU <- 0: jump read_Sj_4
 
@@ -146,29 +187,29 @@ read_Sj_4:
 
 read_Sj_3:
     MDR <- 65280 & MDR
-    MDR <- 0^8@MDR[31..8]
+    MDR <- MDR >> 8
     tmp_Sj <- MDR                   ; needed later for the write functions
-    tmp_Si <- tmp_Si[31-8..0]@0^8   ; shift Si to position 3
+    tmp_Si <- tmp_Si << 8           ; shift Si to position 3
 
 read_Sj_2:
     MDR <- 16711680 & MDR
-    MDR <- 0^16@MDR[31..16]
+    MDR <- MDR >> 16
     tmp_Sj <- MDR                   ; needed later for the write functions
-    tmp_Si <- tmp_Si[31--16..0]@0^16 ;shift Si to position 2
+    tmp_Si <- tmp_Si << 16          ;shift Si to position 2
 
 read_Sj_1:
     MDR <- -16777216 & MDR
-    MDR <- 0^24@MDR[31..24]
+    MDR <- MDR >> 24
     tmp_Sj <- MDR                   ; needed later for the write functions
-    tmp_Si <- tmp_Si[31-24..0]@0^24 ;shift Si to position 1
+    tmp_Si <- tmp_Si << 24             ;shift Si to position 1
 
 ///////////////////////
 
 ; function to shift tmp_Sj to the position where Si was at the beginning
 S.L._tmp_Sj:
-    ACCU <- i mod 4
+    ACCU <- i & 3
 
-    if ACCU <- 0: break
+    if ACCU <- 0: break  ; no need for shifting if it is already at the right position
 
     ACCU <- ACCU - 1
     if ACCU <- 0 : jump S.L._tmp_Sj_8
@@ -179,13 +220,13 @@ S.L._tmp_Sj:
     S.L._tmp_Sj_24
 
 S.L._tmp_Sj_8:
-    tmp_Sj <- tmp_Sj[31-8..0]@0^8
+    tmp_Sj <- tmp_Sj << 8 
 
 S.L._tmp_Sj_16:
-    tmp_Sj <- tmp_Sj[31-16..0]@0^16
+    tmp_Sj <- tmp_Sj << 16 
 
 S.L._tmp_Sj_24:
-    tmp_Sj <- tmp_Sj[31-24..0]@0^24
+    tmp_Sj <- tmp_Sj << 24 
 
 ///////
 ; put the value of Si into the position of Sj aka Swap 1 of 2
@@ -194,7 +235,7 @@ put_Si_into_Sj:
     MDR <- [MAR]
 
     ; find correct memory cell , depending on that call the right write function
-    ACCU <- j MOD 4
+    ACCU <- j & 3
 
     if ACCU <- 0: jump write_Sj_4
 
@@ -210,25 +251,25 @@ put_Si_into_Sj:
 write_Sj_4:
 
     MDR <- -256 & MDR ;delete_byte_4
-    MDR <- tmp_Si | MDR;write tmp_Si in S[j]
+    MDR <- tmp_Si + MDR;write tmp_Si in S[j]
     M[MAR] <- MDR
 
 write_Sj_3:
 
     MDR <- -65281 & MDR  ;delete_byte_3
-    MDR <- tmp_Si | MDR  ;write tmp_Si in S[j]
+    MDR <- tmp_Si + MDR  ;write tmp_Si in S[j]
     M[MAR] <- MDR
 
 write_Sj_2:
 
     MDR <- -16711681 & MDR;delete_byte_2
-    MDR <- tmp_Si | MDR;write tmp_Si in S[j]
+    MDR <- tmp_Si + MDR;write tmp_Si in S[j]
     M[MAR] <- MDR
 
 write_Sj_1:
 
     MDR <- 16777215 & MDR;delete_byte_1
-    MDR <- tmp_Si | MDR;write tmp_Si in S[j]
+    MDR <- tmp_Si + MDR;write tmp_Si in S[j]
     M[MAR] <- MDR
 //////////
 ; put the value of Sj into the position of Si aka Swap 2 of 2
@@ -238,7 +279,7 @@ put_Sj_into_Si:
     MDR <- [MAR]
 
     ; find correct memory cell , depending on that call the right write function
-    ACCU <- i MOD 4
+    ACCU <- i & 3
 
     if ACCU <- 0: jump write_Si_4
 
@@ -254,25 +295,25 @@ put_Sj_into_Si:
 write_Si_4:
 
     MDR <- -256 & MDR;delete_byte_4
-    MDR <- tmp_Sj | MDR;write tmp_Sj in S[i]
+    MDR <- tmp_Sj + MDR;write tmp_Sj in S[i]
     M[MAR] <- MDR
 
 write_Si_3:
 
     MDR <- -65281 & MDR;delete_byte_3
-    MDR <- tmp_Sj | MDR;write tmp_Sj in S[i]
+    MDR <- tmp_Sj + MDR;write tmp_Sj in S[i]
     M[MAR] <- MDR
 
 write_Si_2:
 
     MDR <- -16711681 & MDR;delete_byte_2
-    MDR <- tmp_Sj | MDR;write tmp_Sj in S[i]
+    MDR <- tmp_Sj + MDR;write tmp_Sj in S[i]
     M[MAR] <- MDR
 
 write_Si_1:
 
     MDR <- 16777215 & MDR;delete_byte_4
-    MDR <- tmp_Sj | R MDR;write tmp_Sj in S[j]
+    MDR <- tmp_Sj + R MDR;write tmp_Sj in S[j]
     M[MAR] <- MDR
 
 ////////////////
@@ -290,7 +331,7 @@ shuffle_sbox:
     j <- j + MDR
 
     ; j <- j % 256
-    j <- j MOD 256
+    j <- j & 255
 
     ; load sBox(j)
     get_Sj
@@ -302,7 +343,7 @@ shuffle_sbox:
 
     ; repeat 256 times
     i <- i + 1
-    counter - 1
+    counter <- counter - 1
 
     ; if repeated 256 times, the sbox should be correctly shuffled
     if counter <- 0: break
@@ -322,7 +363,7 @@ decrypt_setup:
     i <- 0
     j <- 0
 
-    ; when wordIndex<<2 is greater than INPUT_LENGTH, we stop iterating
+    ; when wordIndex is greater than INPUT_LENGTH, we stop iterating
     wordIndex <- 0
 
 ///////////////
@@ -344,13 +385,13 @@ decrypt_loop_outer:
 decrypt_loop_inner:
     ; count i clockwise. mod 256, because 256 is the length of the sbox
     i <- i + 1
-    i <- i & 0xff ; 0xff === 255.
+    i <- i & 255;
 
     # i1 is the word-address part of the i-th sbox byte
     # while i2 is the byte position of i-th sbox byte
 
     i1 <- i >> 2
-    i2 <- i & 0b11 ; 0b11 === 3
+    i2 <- i & 3
     i2 <- i2 * 8
 
     ; ibox <- sbox[i]
@@ -363,7 +404,7 @@ decrypt_loop_inner:
 
     ; increment j by the ith byte in the sbox mod 256
     j <- j + ibox
-    j <- j & 0xff; 0xff === 255
+    j <- j & 255
 
     ; the way we've split i up into an address
     ; and a byte positon part,
@@ -372,7 +413,7 @@ decrypt_loop_inner:
     ; j1 is the word address
     j1 <- j >> 2
     ; j2 is the byte position in that word
-    j2 <- j & 0b11
+    j2 <- j & 3
     j2 <- j2 * 8
 
     ; index <- sbox[j]
@@ -383,11 +424,8 @@ decrypt_loop_inner:
     ; bring relevant byte at position 0..8
     index <- ACCU >> j2
     index <- index + ibox
-    index <- index & 0xff
+    index <- index & 255
 
-    ; ibox is no longer needed
-
-    ; BREAKPOINT A
     
     ; now mutate the sbox, by swapping the ith and the jth byte
     swap_byte_start
@@ -409,37 +447,35 @@ swap_same_word:
     ; if we're operating on the same byte, abort
     ACCU <- i2 - j2
     if ACCU === 0: jump: swap_byte_end
+
     ; we're operating on the same word, but on different bytes
-
-    ; we're reusing ibox now
-
     MAR <- SBOX_ADDR + i1
     ibox <- MDR[MAR]
 
     ; get i2 byte, move at j2 position
     a <- ibox >> i2
-    a <- a & 0xff
+    a <- a & 255
     a <- a << j2
 
     ; get j2 byte, move at i2 position
     ; we will use register j1, it is no longer needed.
     j1 <- ibox >> j2
-    j1 <- j1 & 0xff
+    j1 <- j1 & 255
     j1 <- j1 << i2
 
     ; merge both bytes.
     ; They are guaranteed to be at separate locations.
-    a <- a | b
+    a <- a + b
 
     ; j1 is no longer needed
 
     ; j1 will have 1s on the byte addressed by j2
-    j1 <- 0xff
+    j1 <- 255
     j1 <- j1 << j2
     ; ACCU will have 1s on the byte addressed by i1
-    ACCU <- 0xff << i2
+    ACCU <- 255 << i2
     ; merge 1s of j1 and ACCU
-    ACCU <- ACCU | j1
+    ACCU <- ACCU + j1
     ACCU <- INV ACCU
     ; ACCU now has a 1 every where, except where the 2 byte lie,
     ; that should be switched around
@@ -448,7 +484,7 @@ swap_same_word:
     ibox <- ibox & ACCU
 
     ; and now set them
-    ibox <- ibox | a
+    ibox <- ibox + a
 
     ; write back to the sbox
     MAR <- SBOX_ADDR + i1
@@ -466,25 +502,25 @@ swap_differing_word:
     MAR <- SBOX_ADDR + i1
     a <- MDR[MAR]
     a <- a >> i2
-    a <- a & 0xff
+    a <- a & 255
     ; move a at position it's needed
     a <- a << j2
 
     MAR <- SBOX_ADDR + j1
     b <- MDR[MAR]
     b <- b >> j2
-    b <- b & 0xff
+    b <- b & 255
     ; move b at position it's needed
     b <- b << i2
 
     MAR <- SBOX_ADDR + i1
     ibox <- MDR[MAR]
-    ACCU <- 0xff << i2
+    ACCU <- 255 << i2
     ACCU <- INV ACCU
     ; clear byte at pos of a in ibox
     ibox <- ibox & ACCU
     ; set byte b
-    ibox <- ibox | b
+    ibox <- ibox + b
     ; write back to memory
     MDR[MAR] <- ibox
 
@@ -492,11 +528,11 @@ swap_differing_word:
     MAR <- SBOX_ADDR + j1
     ibox <- MDR[MAR]
     ; clear the position byte b will occupy in ibox
-    ACCU <- 0xff << j2
+    ACCU <- 255 << j2
     ACCU <- INV ACCU
     ibox <- ibox & ACCU
     ; set byte a
-    ibox <- ibox | a
+    ibox <- ibox + a
     ; write back to memory
     MDR[MAR] <- ibox
 
@@ -514,16 +550,16 @@ swap_byte_end:
     ibox <- MDR[MAR]
 
     ; extract the byte position from index
-    index <- index & 0b11; 0b11 === 3
+    index <- index & 3
     index <- index * 8
 
     ; single out the byte we want from the word
     ibox <- ibox >> index
-    ibox <- ibox & 0xff
+    ibox <- ibox & 255
 
     ; apply the byte to the 4 byte xor pattern
     ibox <- ibox << loopIndex
-    pattern <- pattern | ibox
+    pattern <- pattern + ibox
 
     ;   -----------------------------
     ;   --- check inner loop conditions ---
@@ -537,7 +573,7 @@ swap_byte_end:
 ///////////////
 
 perform_xor:
-    ; BREAKPOINT B
+
     ; analyze pattern here
 
     ; xor 4 bytes at once, by leveraging word based instructions
@@ -566,7 +602,3 @@ perform_xor:
 ///////////////
 
 end:
-
-; TODO if last byte does not get transformed,
-; incremet wordIndex by 1
-; TODO replace all | with +, to drive down `cost`
